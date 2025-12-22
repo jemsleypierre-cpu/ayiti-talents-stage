9d4e6e76d67b239b3cfdb2354f816dd941e9af8e
Jemsley Pierre <jemsleypierre@gmail.com>
2025-12-21 22:04:53 -0500

    cursor edit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/RAYON-DES-JEUNES-TALENTS/",
   server: {
     host: "::",
     port: 8080,
